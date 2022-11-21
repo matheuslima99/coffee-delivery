e021ff31bc3a8fab940327aceb27d0baeaa3d5cd
matheuslima99 <maths.soares99@gmail.com>
2022-11-20 22:10:41 -0300

    Application features - Displayng data on component card

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -12,29 +12,44 @@ import {
   TagsCoffeeContainer,
 } from "./styles";
 
-export function CoffeeCard() {
+export interface Coffee {
+  id: number;
+  tags: string[];
+  name: string;
+  description: string;
+  photo: string;
+  price: number;
+}
+
+interface CoffeeCardProps {
+  coffee: Coffee;
+}
+
+export function CoffeeCard({ coffee }: CoffeeCardProps) {
   return (
     <CoffeeCardContainer>
       <img src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1669593600&Signature=EOGbXQYBeqYX1C7GcMygyM0XJaqbOU3ydZiPqL-NY6aHiskkr5FHMDc8QkVgOtGsiimXJJcmgjCy6XxHHUQcEHd1acupWJNluTrZXcnUpXvk0n6xEb5bBv6nQrioxaAGeaBA1L9Rd35dPGSi2ciwfqNKI7EctYFJ1qwJglnEqKW8bylgmjPXr6IEAXuZ2f4gRujfM~ikF64yMxA8i1TBrnpm4iPJqQ8dKZy3ncRYbjDnkyYWX-ngs51EbpbIkS5gsWuiafbgBmdCqEuso-t~lO0noqAiQ9lAS7mdfhvqrAsclTTn8gIXaDm--CciazC0p83m03Mf3YqLy-M9Sin8QA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
       <TagsCoffeeContainer>
-        <TagCoffee>
-          <TagCoffeeText>Tradicional</TagCoffeeText>
-        </TagCoffee>
+        {coffee.tags.map((tag) => (
+          <TagCoffee key={`${coffee.id}${tag}`}>
+            <TagCoffeeText>{tag}</TagCoffeeText>
+          </TagCoffee>
+        ))}
       </TagsCoffeeContainer>
 
       <CoffeeName size="s" color="subtitle" as="h3">
-        Expresso Tradicional
+        {coffee.name}
       </CoffeeName>
 
       <CoffeeDescription size="s" color="label">
-        O tradicional café feito com água quente e grãos moídos
+        {coffee.description}
       </CoffeeDescription>
 
       <CoffeeCardFooter>
         <div>
           <RegularText size="s">R$</RegularText>
           <TitleText color="text" as="strong">
-            9,90
+            {coffee.price}
           </TitleText>
         </div>
 
