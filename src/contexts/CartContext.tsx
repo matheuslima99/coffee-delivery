f85fd4dc65e2cd362854d9f5bf78e6be59337750
matheuslima99 <maths.soares99@gmail.com>
2022-11-26 02:45:50 -0300

    Contexts - clearCart function (useCart)

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -15,6 +15,7 @@ interface CartContextType {
     type: "increase" | "decrease"
   ) => void;
   removeCartItem: (cartItemId: number) => void;
+  cleanCart: () => void;
 }
 
 interface CartContextProviderProps {
@@ -89,6 +90,10 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     );
   }
 
+  function cleanCart() {
+    setCartItems([]);
+  }
+
   useEffect(() => {
     localStorage.setItem(COFFEE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems));
   }, [cartItems]);
@@ -102,6 +107,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
         addCoffeeToCart,
         changeCartItemQuantity,
         removeCartItem,
+        cleanCart,
       }}
     >
       {children}
--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -5,6 +5,7 @@ import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, FormProvider } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 
 enum PaymentMethods {
   credit = "credit",
@@ -38,12 +39,16 @@ export function CompleteOrder() {
 
   const { handleSubmit } = confirmOrderForm;
 
+  const { cleanCart } = useCart();
+
   const navigate = useNavigate();
 
   function handleConfirmOrder(data: ConfirmOrderFormData) {
     navigate("/orderConfirmed", {
       state: data,
     });
+
+    cleanCart();
   }
 
   return (
